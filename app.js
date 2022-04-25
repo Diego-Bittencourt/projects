f9a641939cda49351712c6a247dfe9e7a8b264ce
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-04-25 11:35:43 +0900

    Implemented the JS file and create the item add process functional. From now on, it is important to finalize the styling and add more functionalities.

---
+++ app.js
@@ -0,0 +1,14 @@
+var user_input = document.getElementById("insertText");
+const button = document.getElementById("insertButton");
+const list = document.getElementById("goal-list");
+
+console.log(user_input);
+
+button.addEventListener("click", addList);
+
+function addList() {
+    let new_item = document.createElement("p");
+    list.appendChild(new_item);
+    new_item.innerText = user_input.value; //The .value didn't work after the getElementById, but it worked after the variable's name.
+    user_input.value = "";
+}
