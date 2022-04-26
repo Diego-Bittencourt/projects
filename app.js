5714e9f31a85dea1ba58d20e1d307575f136b7a3
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-04-26 16:19:33 +0900

    Change the "remove" function and change it to "toggle" "To do"/"Done!" It was created the red "To do" indicator and after clicking on it, it changes its color from red to green and the text changes to "Done!". Algo Added the filter to only add items to the list if the input is different from an empty string.

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 var user_input = document.getElementById("insertText");
 const button = document.getElementById("insertButton");
 const list = document.getElementById("goal-list");
+var items = document.getElementsByTagName("span");
 
-console.log(user_input);
+
 
 button.addEventListener("click", addList);
 
 function addList() {
-    let new_item = document.createElement("p");
+    if(user_input.value === "") {
+        console.log("empty input") //substitute this line to a function to shake the input element
+        return
+    }
+    let new_item = document.createElement("p")
     list.appendChild(new_item);
-    new_item.innerText = user_input.value; //The .value didn't work after the getElementById, but it worked after the variable's name.
+    console.log(user_input.value); //remove later. It's here just to test
+    new_item.innerHTML = "<span>To do</span>" + user_input.value; //The .value didn't work after the getElementById, but it worked after the variable's name.
     user_input.value = "";
+    for (let i = 0; i < items.length; i++) {
+        items[i].onclick = list_remove;
+    }
 }
 
-var clicked_item = document.getElementsByTagName("p");
-clicked_item.addEventListener("click", remove(element))
+function list_remove(item_created) {
+    let item = item_created.target;
+    item.classList.add("done");
+    item.innerText = "Done!";
+   // item.remove(); // let's put this in stand by for a while.
+   
+}
